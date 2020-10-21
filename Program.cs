6aaae01ee24b1f396cb2557c8467107f40315167
Amarotte <71397345+Amarotte@users.noreply.github.com>
2020-10-21 18:00:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace oop_week6_shoppingcart
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+
+            //Classic example without Classes
+            double totalshopping = 0;
+            List<string> ShoppingToday = new List<string> { "apple", "orange","orange","apple","apple" }; // sum = 2.6
+            foreach(string element in ShoppingToday)
+            {
+                if(element=="apple")
+                {
+                    totalshopping += 0.6;
+                }
+                else { totalshopping += 0.4; }
+            }
+            Console.WriteLine(totalshopping + " to pay please");         
+        }
+        
+    }
+}
---
+++ ShoppingCart.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_week6_shoppingcart
+{
+    public class ShoppingCart 
+    {
+        List<double> Price;
+        List<string> Courses;
+
+        public double applePricee = 0.6;
+        public double OrangePrice = 0.4;
+
+        public void AddApple()
+        {
+            Courses.Add("apple");
+        }
+
+        public void AddOrange()
+        {
+            Courses.Add("orange");
+        }
+
+        public double PriceShoppingCart()
+        {
+            double total=0;
+
+            foreach (string element in Courses)
+            {
+                if (element == "apple")
+                {
+                    total += 0.6;
+                }
+                else { total += 0.4; }
+            }
+
+            return total;
+        }
+
+    }
+}
